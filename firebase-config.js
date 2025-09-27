144d8f45487907fa4417ef37fc7fbe0fd69b0b2d
Mr-rabbit-xd <bihh5539@gmail.com>
2025-09-27 09:16:57 +0530

    Update and rename firebase-Config.js to firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, get } from "firebase/database";
 
+// তোমার Firebase config দিয়ে replace করো
 const firebaseConfig = {
   apiKey: "AIzaSyDOJRWTrO3A7_0SqvBxsCTt6bkNo8pV-bc",
   authDomain: "vcfweb-9bd1f.firebaseapp.com",
   databaseURL: "https://vcfweb-9bd1f-default-rtdb.firebaseio.com",
   projectId: "vcfweb-9bd1f",
-  storageBucket: "vcfweb-9bd1f.firebasestorage.app",
+  storageBucket: "vcfweb-9bd1f.appspot.com",
   messagingSenderId: "204316192596",
-  appId: "1:204316192596:android:5b5d6052167c5dd270a2c1"
+  appId: "1:204316192596:web:5b5d6052167c5dd270a2c1"
 };
 
 const app = initializeApp(firebaseConfig);
